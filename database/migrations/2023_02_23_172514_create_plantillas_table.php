29efa390690f8ad5a218ebf3cdffe7789cff5146
Itsgabriel <gabrielasimbaya82@gmail.com>
2023-02-25 11:38:00 -0500

    Arregnadlo campos de tablas

--- database/migrations/2023_02_23_172514_create_plantillas_table.php
+++ database/migrations/2023_02_23_172514_create_plantillas_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('plantillas', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('team_id')->constrained('teams');
             $table->timestamps();
         });
     }
