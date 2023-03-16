eacfc11c022428a94c1814b1b75dc24ead41177b
Itsgabriel <gabrielasimbaya82@gmail.com>
2023-03-16 02:39:15 -0500

    Agregando referencias

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(RoleSeeder::class);
-
-         \App\Models\User::factory(10)->create();
-
-         \App\Models\User::factory()->create([
+       
+        \App\Models\User::factory()->create([
              'name' => 'Test User',
              'email' => 'test@example.com',
              'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
@@ -39,19 +37,6 @@ class DatabaseSeeder extends Seeder
              'email' => 'joelpro@example.com',
              'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
          ])->assignRole('representante');
-
-        \App\Models\Sancion::create([
-            'tipo' => 'roja'
-         ]);
-    
-        \App\Models\Sancion::create([
-            'tipo' => 'ninguna'
-        ]);
-
-        \App\Models\Sancion::create([
-            'tipo' => 'amarilla'
-        ]);
-
-        
+ 
     }
 }
